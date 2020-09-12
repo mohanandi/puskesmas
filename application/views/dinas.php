2debab9346868099c0c10693b62bdaf66e369cbe
aldinmo <aldinmoehammad1892@gmail.com>
2020-09-12 18:16:26 +0700

    Page dinas updated

--- application/views/dinas.php
+++ application/views/dinas.php
@@ -1,219 +1,91 @@
-<div class="site-section" id="menu">
-    <div class="container">
+<link rel="stylesheet" type="text/css" href="<?= base_url('assets/') ?>css/tooltip.css"> 
+
+<div class="site-section" id = "menu">
+      <div class="container">
         <div class="row mb-5">
-            <div class="col-lg-7 mx-auto text-center">
-                <span class="subheading">Form Menu</span>
-                <h2 class="heading"><strong class="text-primary">Identitas Umum</strong> Puskesmas Percontohan</h2>
-            </div>
-        </div>
-        <div class="container1">
-            <div class='multitab-section'>
-                <ul class='multitab-widget multitab-widget-content-tabs-id'>
-                    <li class='multitab-tab'><a href='#multicolumn-widget-id1'>Popular</a></li>
-                    <li class='multitab-tab'><a href='#multicolumn-widget-id2'>Tags</a></li>
-                    <li class='multitab-tab'><a href='#multicolumn-widget-id3'>Archive</a></li>
-                    <li class='multitab-tab'><a href='#multicolumn-widget-id4'>Archive</a></li>
-                    <li class='multitab-tab'><a href='#multicolumn-widget-id5'>Archive</a></li>
-                    <li class='multitab-tab'><a href='#multicolumn-widget-id6'>Archive</a></li>
-                </ul>
-                <div class='multitab-widget-content multitab-widget-content-widget-id' id='multicolumn-widget-id1'>
-                    <span class='sidebar' id='sidebartab1' preferred='yes'>
-                        <p>
-                            <form>
-                                <div class="form-group">
-                                    <label for="exampleFormControlInput1">Nama Puskesmas</label>
-                                    <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="">
-                                </div>
-                                <div class="form-group">
-                                    <label for="exampleFormControlInput1">No. Register</label>
-                                    <input type="number" class="form-control" id="exampleFormControlInput1" placeholder="">
-                                </div>
-                                <div class="form-group">
-                                    <label for="exampleFormControlInput1">Tanggal Pendirian</label>
-                                    <input type="date" class="form-control" id="exampleFormControlInput1" placeholder="">
-                                </div>
-                                <div class="form-group">
-                                    <label for="exampleFormControlInput1">Alamat</label>
-                                    <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="">
-                                </div>
-                                <div class="form-group">
-                                    <label for="exampleFormControlInput1">Kecamatan</label>
-                                    <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="">
-                                </div>
-                                <div class="form-group">
-                                    <label for="exampleFormControlInput1">Kabupaten/Kota</label>
-                                    <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="">
-                                </div>
-                                <div class="form-group">
-                                    <label for="exampleFormControlInput1">Provinsi</label>
-                                    <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="">
-                                </div>
-                                <div class="form-group">
-                                    <label for="exampleFormControlInput1">No. Telepon Puskesmas dan No Telepon Whatsapp</label>
-                                    <input type="number" class="form-control" id="exampleFormControlInput1" placeholder="">
-                                </div>
-                                <div class="form-group">
-                                    <label for="exampleFormControlInput1">No. Telepon Ruang Gadar</label>
-                                    <input type="number" class="form-control" id="exampleFormControlInput1" placeholder="">
-                                </div>
-                                <div class="form-group">
-                                    <label for="exampleFormControlInput1">No. Faksimile</label>
-                                    <input type="number" class="form-control" id="exampleFormControlInput1" placeholder="">
-                                </div>
-                                <div class="form-group">
-                                    <label for="exampleFormControlInput1">Alamat email dan website</label>
-                                    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="">
-                                </div>
-                                <a class="btn" href="table.html" style="background-color: #16b3ac; color: white;">Submit</a>
-                            </form></br>
-                    </span>
-                </div>
-                <div class='multitab-widget-content multitab-widget-content-widget-id' id='multicolumn-widget-id2'>
-                    <span class='sidebar' id='sidebartab2' preferred='yes'>
-                        <p>Isi konten widget tab 2
-                            </br>
-                            </br>
-                            Bisa sobat isi dengan widget apa saja, misal widget popular post, label, arsip, dll.</br>
-                    </span>
-                </div>
-                <div class='-content multitab-widget-content-widget-id' id='multicolumn-widget-id3'>
-                    <span class='sidebar' id='sidebartab3' preferred='yes'>
-                        <p>Isi konten widget tab 3
-                            </br>
-                            </br>
-                            Bisa sobat isi dengan widget apa saja, misal widget popular post, label, arsip, dll.</br>
-                    </span>
-                </div>
-                <div class='-content multitab-widget-content-widget-id' id='multicolumn-widget-id4'>
-                    <span class='sidebar' id='sidebartab3' preferred='yes'>
-                        <p>Isi konten widget tab 3
-                            </br>
-                            </br>
-                            Memek lah.</br>
-                    </span>
-                </div>
-                <div class='-content multitab-widget-content-widget-id' id='multicolumn-widget-id5'>
-                    <span class='sidebar' id='sidebartab3' preferred='yes'>
-                        <p>Isi konten widget tab 3
-                            </br>
-                            </br>
-                            <table id="example" class="table table-striped table-bordered" style="width:100%;  text-align: center; vertical-align: middle;">
-                                <thead>
-                                    <tr>
-                                        <th>No</th>
-                                        <th></th>
-                                        <th></th>
-                                        <th>Edit</th>
-                                        <th>Hapus</th>
-                                        <th>Verifikasi</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr>
-                                        <td>1</td>
-                                        <td>Nama Puskesmas</td>
-                                        <td> - </td>
-                                        <td> <a href='#'><i class="fa fa-pencil-square-o"></i></a></td>
-                                        <td><a href='#' style="color: red;"><i class="fa fa-trash-o"></i></a></td>
-                                        <td><input type='checkbox'></td>
-
-                                    </tr>
-                                    <tr>
-                                        <td>2</td>
-                                        <td>No. Register</td>
-                                        <td> - </td>
-                                        <td> <a href='#'><i class="fa fa-pencil-square-o"></i></a></td>
-                                        <td><a href='#' style="color: red;"><i class="fa fa-trash-o"></i></a></td>
-                                        <td><input type='checkbox'></td>
-                                    </tr>
-                                    <tr>
-                                        <td>3</td>
-                                        <td>Tanggal Pendirian</td>
-                                        <td> - </td>
-                                        <td> <a href='#'><i class="fa fa-pencil-square-o"></i></a></td>
-                                        <td><a href='#' style="color: red;"><i class="fa fa-trash-o"></i></a></td>
-                                        <td><input type='checkbox'></td>
-                                    </tr>
-                                    <tr>
-                                        <td>4</td>
-                                        <td>Alamat</td>
-                                        <td> - </td>
-                                        <td> <a href='#'><i class="fa fa-pencil-square-o"></i></a></td>
-                                        <td><a href='#' style="color: red;"><i class="fa fa-trash-o"></i></a></td>
-                                        <td><input type='checkbox'></td>
-                                    </tr>
-                                    <tr>
-                                        <td>5</td>
-                                        <td>Kecamatan</td>
-                                        <td> - </td>
-                                        <td> <a href='#'><i class="fa fa-pencil-square-o"></i></a></td>
-                                        <td><a href='#' style="color: red;"><i class="fa fa-trash-o"></i></a></td>
-                                        <td><input type='checkbox'></td>
-                                    </tr>
-                                    <tr>
-                                        <td>6</td>
-                                        <td>Kabupaten/Kota</td>
-                                        <td> - </td>
-                                        <td> <a href='#'><i class="fa fa-pencil-square-o"></i></a></td>
-                                        <td><a href='#' style="color: red;"><i class="fa fa-trash-o"></i></a></td>
-                                        <td><input type='checkbox'></td>
-                                    </tr>
-                                    <tr>
-                                        <td>7</td>
-                                        <td>Provinsi</td>
-                                        <td> - </td>
-                                        <td> <a href='#'><i class="fa fa-pencil-square-o"></i></a></td>
-                                        <td><a href='#' style="color: red;"><i class="fa fa-trash-o"></i></a></td>
-                                        <td><input type='checkbox'></td>
-                                    </tr>
-                                    <tr>
-                                        <td>8</td>
-                                        <td>No. Telepon Puskesmas dan Whatsapp</td>
-                                        <td> - </td>
-                                        <td> <a href='#'><i class="fa fa-pencil-square-o"></i></a></td>
-                                        <td><a href='#' style="color: red;"><i class="fa fa-trash-o"></i></a></td>
-                                        <td><input type='checkbox'></td>
-                                    </tr>
-                                    <tr>
-                                        <td>9</td>
-                                        <td>No. Telpon Ruang Gadar</td>
-                                        <td> - </td>
-                                        <td> <a href='#'><i class="fa fa-pencil-square-o"></i></a></td>
-                                        <td><a href='#' style="color: red;"><i class="fa fa-trash-o"></i></a></td>
-                                        <td><input type='checkbox'></td>
-                                    </tr>
-                                    <tr>
-                                        <td>10</td>
-                                        <td>No. Faksimile</td>
-                                        <td> - </td>
-                                        <td> <a href='#'><i class="fa fa-pencil-square-o"></i></a></td>
-                                        <td><a href='#' style="color: red;"><i class="fa fa-trash-o"></i></a></td>
-                                        <td><input type='checkbox'></td>
-                                    </tr>
-                                    <tr>
-                                        <td>11</td>
-                                        <td>Alamat e-mail dan website</td>
-                                        <td> - </td>
-                                        <td> <a href='#'><i class="fa fa-pencil-square-o"></i></a></td>
-                                        <td><a href='#' style="color: red;"><i class="fa fa-trash-o"></i></a></td>
-                                        <td><input type='checkbox'></td>
-                                    </tr>
-
-
-                                </tbody>
-                            </table></br>
-                    </span>
-                </div>
-                <div class='-content multitab-widget-content-widget-id' id='multicolumn-widget-id6'>
-                    <span class='sidebar' id='sidebartab3' preferred='yes'>
-                        <p>Isi konten widget tab 3
-                            </br>
-                            </br>
-                            Bisa sobat isi dengan widget apa saja, misal widget popular post, label, arsip, dll.</br>
-                    </span>
-                </div>
-            </div>
+          <div class="col-lg-7 mx-auto text-center">
+            <span class="subheading">Form Menu</span>
+            <h2 class="heading"><strong class="text-primary">Pemenuhan Sumber Daya</strong> Puskesmas</h2>
+          </div>
         </div>
 
-    </div>
+        <div class="row">
+  <div class="col-md-3"> 
+</head>
+<body>
+<a id="button"></a>
+<button type="button active" class="collapsible">Profil Puskesmas</button>
+<div class="content">
+  <br>
+  <img src="<?= base_url('assets/') ?>images/person_3.jpg" alt="Avatar" style="width:150px; border-radius: 50%; margin-left: 15%;">
+  <br>
+  <br>
+    <p style="text-align: center;">Nama Puskesmas <span><strong>Puskesmas Jakarta</strong></span></p>
+    <p style="text-align: center;">Lokasi<br> <span><strong>Pondok Labu, Jakarta Selatan No. 17</strong></span></p>
+    <p style="text-align: center;">Tanggal Peresmian<br> <span><strong>20 Juni 2011</strong></span></p>
+    <p style="text-align: center;">Status Akreditasi<br><span><strong>Paripurna</strong></span></p>
+    <p style="text-align: center;">Motto<br> <span><strong>Jaya Selalu Puskesmas</strong></span></p>
 </div>
+<br>
+
+<p style="text-align: center;">Menu Cepat</p>
+<button type="button" class="collapsible">Penilaian Puskesmas</button>
+<div class="content">
+  <br>
+  <a href="#"><p style="text-align: center;">Pemenuhan Sumber Daya Puskesmas</p></a>
+  <hr>
+   <a href="#"><p style="text-align: center;">Perencanaan Puskesmas</p></a>
+   <hr>
+    <a href="#"><p style="text-align: center;">Penggerakan dan Pelaksanaan Sumber Daya Puskesmas</p></a>
+    <hr>
+     <a href="#"><p style="text-align: center;">Pengawasan, Pengendalian dan Penilaian Kinerja Puskesmas</p></a>
+     <hr>
+      <a href="#"><p style="text-align: center;">Peningkatan Mutu Puskesmas</p></a>
+      <hr>
+      <a href="#"><p style="text-align: center;">Pencegahan dan Pengendalian Infkesi dan Kesehatan Lingkungan</p></a>
+      <hr>
+      <a href="#"><p style="text-align: center;">Pelaksanaan Sistem Kewaspadaan Dini dan Respons Terhadap Penyakit Menular Potensial KLB/Wabah</p></a>
+      <hr>
+      <a href="#"><p style="text-align: center;">Cakupan Indikator Program</p></a>
+      <hr>
+      <a href="#"><p style="text-align: center;">Upaya Inovasi</p></a>
+      <hr>
+      <a href="#"><p style="text-align: center;">Rekapitulasi Skor</p></a>
+      <hr>
+      <a href="#"><p style="text-align: center;">Rencana Tidak Lanjut</p></a>
+
+</div>
+<button type="button" class="collapsible">Menu 2</button>
+<div class="content">
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+</div>
+<button type="button" class="collapsible">Menu 3</button>
+<div class="content">
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+</div>
+</div>
+  <div class="col-md-9"> 
+    <form>
+  <div class="form-group">
+    <label for="exampleFormControlInput1">Pemenuhan standar bangunan Puskesmas <span data-tooltip="Persentase pemenuhan standar bangunan Puskesmas yang tercantum dalam ASPAK dan sudah divalidasi pada saat pembinaan" style="margin-left: 3px; font-size: 12px; color: #16b3ac;"> Kriteria </span> </label>
+     <select class="form-control">
+       <option selected>Pilih ...</option>
+      <option value="1">≥ 80%</option>
+      <option value="2">61% - 79%</option>
+      <option value="2">≤ 60%</option>
+    </select>
+  </div>
+  <a class="btn" href="table.html" style="background-color: #16b3ac; color: white;">Submit</a>
+  
+</form>
+</div>
+</div>
+
+       
+      </div>
+    </div> 
+  </div>
+</div>
+    </div>
+
+	<script src="<?= base_url('assets/') ?>js/tooltip.js"></script>
