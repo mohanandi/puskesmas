4ef4eac978919867070beacea9fc39b56c098b93
Andi Rahmadi <andimohan2601@gmail.com>
2020-09-14 17:43:18 +0700

    .

--- application/controllers/Identitas_puskesmas.php
+++ application/controllers/Identitas_puskesmas.php
@@ -5,8 +5,17 @@ class Identitas_Puskesmas extends CI_Controller
 {
     public function index()
     {
-        $this->load->view('templates/form_table/head');
+        $this->load->view('templates/puskesmas/head');
         $this->load->view('puskesmas/identitas_puskesmas');
-        $this->load->view('templates/form_table/foot');
+        $this->load->view('templates/puskesmas/foot');
+    }
+
+    public function tambah()
+    {
+        var_dump($this->input->post());
+        $this->form_validation->set_rules('nama', 'Nama', 'required');
+        $this->form_validation->set_rules('bangunan', 'Bangunan', 'required|in_list[Ya,Tidak]');
+        $this->form_validation->set_rules('nrp', 'NRP', 'required|numeric');
+        $this->form_validation->set_rules('email', 'Email', 'required|valid_email');
     }
 }
--- application/views/puskesmas/identitas_puskesmas.php
+++ application/views/puskesmas/identitas_puskesmas.php
@@ -1,13 +1,13 @@
 <link rel="stylesheet" type="text/css" href="<?= base_url('assets/') ?>css/tooltip.css">
 <div class="site-section" id="menu">
-    <div class="container">
-        <div class="row mb-5">
-            <div class="col-lg-7 mx-auto text-center">
-                <span class="subheading">Form Menu</span>
-                <h2 class="heading"><strong class="text-primary">Identitas Umum</strong> Puskesmas Percontohan</h2>
-            </div>
-        </div>
-	<div class="row">
+  <div class="container">
+    <div class="row mb-5">
+      <div class="col-lg-7 mx-auto text-center">
+        <span class="subheading">Form Menu</span>
+        <h2 class="heading"><strong class="text-primary">Identitas Umum</strong> Puskesmas Percontohan</h2>
+      </div>
+    </div>
+    <div class="row">
       <div class="col-md-3">
         </head>
 
@@ -87,59 +87,58 @@
       </div>
       <div class="col-md-9">
 
-        <form style="height:350px; overflow-y:auto; padding:20px;">
-            <div class="form-group">
-                <label for="exampleFormControlInput1">Nama Puskesmas</label>
-                <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="">
-            </div>
-            <div class="form-group">
-                <label for="exampleFormControlInput1">No. Register</label>
-                <input type="number" class="form-control" id="exampleFormControlInput1" placeholder="">
-            </div>
-            <div class="form-group">
-                <label for="exampleFormControlInput1">Tanggal Pendirian</label>
-                <input type="date" class="form-control" id="exampleFormControlInput1" placeholder="">
-            </div>
-            <div class="form-group">
-                <label for="exampleFormControlInput1">Alamat</label>
-                <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="">
-            </div>
-            <div class="form-group">
-                <label for="exampleFormControlInput1">Kecamatan</label>
-                <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="">
-            </div>
-            <div class="form-group">
-                <label for="exampleFormControlInput1">Kabupaten/Kota</label>
-                <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="">
-            </div>
-            <div class="form-group">
-                <label for="exampleFormControlInput1">Provinsi</label>
-                <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="">
-            </div>
-            <div class="form-group">
-                <label for="exampleFormControlInput1">No. Telepon Puskesmas dan No Telepon Whatsapp</label>
-                <input type="number" class="form-control" id="exampleFormControlInput1" placeholder="">
-            </div>
-            <div class="form-group">
-                <label for="exampleFormControlInput1">No. Telepon Ruang Gadar</label>
-                <input type="number" class="form-control" id="exampleFormControlInput1" placeholder="">
-            </div>
-            <div class="form-group">
-                <label for="exampleFormControlInput1">No. Faksimile</label>
-                <input type="number" class="form-control" id="exampleFormControlInput1" placeholder="">
-            </div>
-            <div class="form-group">
-                <label for="exampleFormControlInput1">Alamat email dan website</label>
-                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="">
-            </div>
-          
+        <form style="height:350px; overflow-y:auto; padding:20px;" method="POST" action="<?= base_url('Identitas_Puskesmas/tambah'); ?>">
+          <div class="form-group">
+            <label for="exampleFormControlInput1">Nama Puskesmas</label>
+            <input type="text" class="form-control" id="nama" name="nama" placeholder="">
+          </div>
+          <div class="form-group">
+            <label for="exampleFormControlInput1">No. Register</label>
+            <input type="number" class="form-control" id="no_reg" name="no_reg" placeholder="">
+          </div>
+          <div class="form-group">
+            <label for="exampleFormControlInput1">Tanggal Pendirian</label>
+            <input type="date" class="form-control" id="tgl_diri" name="tgl_diri" placeholder="">
+          </div>
+          <div class="form-group">
+            <label for="exampleFormControlInput1">Alamat</label>
+            <input type="text" class="form-control" id="alamat" name="alamat" placeholder="">
+          </div>
+          <div class="form-group">
+            <label for="exampleFormControlInput1">Kecamatan</label>
+            <input type="text" class="form-control" id="kecamatan" name="kecamatan" placeholder="">
+          </div>
+          <div class="form-group">
+            <label for="exampleFormControlInput1">Kabupaten/Kota</label>
+            <input type="text" class="form-control" id="kab_kota" name="kab_kota" placeholder="">
+          </div>
+          <div class="form-group">
+            <label for="exampleFormControlInput1">Provinsi</label>
+            <input type="text" class="form-control" id="provinsi" name="provinsi" placeholder="">
+          </div>
+          <div class="form-group">
+            <label for="exampleFormControlInput1">No. Telepon Puskesmas dan No Telepon Whatsapp</label>
+            <input type="number" class="form-control" id="no_telp" name="no_telp" placeholder="">
+          </div>
+          <div class="form-group">
+            <label for="exampleFormControlInput1">No. Telepon Ruang Gadar</label>
+            <input type="number" class="form-control" id="telp_gadar" name="telp_gadar" placeholder="">
+          </div>
+          <div class="form-group">
+            <label for="exampleFormControlInput1">No. Faksimile</label>
+            <input type="number" class="form-control" id="faks" name="faks" placeholder="">
+          </div>
+          <div class="form-group">
+            <label for="exampleFormControlInput1">Alamat email dan website</label>
+            <input type="email" class="form-control" id="email" name="email" placeholder="">
+          </div>
+          <button type="submit" class="btn float-right btn-success" href="table.html" style="color: white; margin-left:10px; margin-top:5px; border-radius:5px;">Masukan</button>
         </form>
 
-		<a class="btn float-right btn-success" href="table.html" style="color: white; margin-left:10px; margin-top:5px; border-radius:5px;">Masukan</a>
-		<a class="btn float-right btn-danger" href="#" style="color: white; margin-left:10px; margin-top:5px;border-radius:5px;">Hapus</a>
 
+
+      </div>
     </div>
-</div>
-<script src="<?= base_url('assets/') ?>js/tooltip.js"></script>
+    <script src="<?= base_url('assets/') ?>js/tooltip.js"></script>
 
-<script src="<?= base_url('assets/') ?>js/para.js"></script>
+    <script src="<?= base_url('assets/') ?>js/para.js"></script>
