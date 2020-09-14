3a9f5f0f60c48197c6f9e573637914c5c755f729
Andi Rahmadi <andimohan2601@gmail.com>
2020-09-14 15:43:42 +0700

    .

---
+++ application/controllers/Identitas_puskesmas.php
@@ -0,0 +1,10 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Identitas_puskesmas extends CI_Controller
+{
+    public function index()
+    {
+        $this->load->view('home');
+    }
+}
