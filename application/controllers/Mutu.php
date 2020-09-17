1a6dbeb5a853bfc6222d3ce4ca87bb7a37ace923
Andi Rahmadi <andimohan2601@gmail.com>
2020-09-18 01:16:30 +0700

    .

--- application/controllers/Mutu.php
+++ application/controllers/Mutu.php
@@ -9,33 +9,72 @@ class Mutu extends CI_Controller
         $this->load->model('Mutu_Model');
         is_logged_in();
     }
+
     public function index()
     {
+        $data['judul'] = 'Peningkatan Mutu Puskesmas';
+        $data['table'] = 'peningkatan_mutu';
+        $data['link'] = 'Mutu';
+        $data['fungsi'] = 'mutu';
+        $data['user'] = $this->db->get_where('user', ['kode' => $this->session->userdata('kode')])->row_array();
+        $data['puskesmas'] = $this->Mutu_Model->check_puskesmas($data['user']['kab_kota']);
         $this->load->view('templates/kab_kota/head');
-        $this->load->view('kab_kota/mutu');
+        $this->load->view('kab_kota/table', $data);
+        $this->load->view('templates/kab_kota/foot');
+    }
+
+    public function data($kode)
+    {
+        $data['judul'] = 'Peningkatan Mutu Puskesmas';
+        $data['user'] = $this->db->get_where('user', ['kode' => $this->session->userdata('kode')])->row_array();
+        $data['data'] = $this->Mutu_Model->check($kode);
+        $data['kode_puskesmas'] = $kode;
+        $this->load->view('templates/kab_kota/head', $data);
+        if ($data['data'] == NULL) {
+            $this->load->view('kab_kota/mutu', $data);
+        } else {
+            $this->load->view('kab_kota/mutu_hasil', $data);
+        }
         $this->load->view('templates/kab_kota/foot');
     }
     public function tambah()
     {
-        $this->form_validation->set_rules('peningkatan1', '', 'trim|required');
-        $this->form_validation->set_rules('peningkatan2', '', 'trim|required');
-        $this->form_validation->set_rules('peningkatan3', '', 'trim|required');
-        $this->form_validation->set_rules('peningkatan4', '', 'trim|required');
-        $this->form_validation->set_rules('peningkatan5', '', 'trim|required');
+        $this->form_validation->set_rules('kode_puskesmas', '', 'trim|required');
+        $this->form_validation->set_rules('peningkatan1', '', 'trim|in_list[1,2,3]');
+        $this->form_validation->set_rules('peningkatan2', '', 'trim|in_list[1,2,3]');
+        $this->form_validation->set_rules('peningkatan3', '', 'trim|in_list[1,2,3]');
+        $this->form_validation->set_rules('peningkatan4', '', 'trim|in_list[1,2,3]');
+        $this->form_validation->set_rules('peningkatan5', '', 'trim|in_list[1,2,3]');
 
         if ($this->form_validation->run() == false) {
-            $this->index();
+            $this->data($this->input->post('kode_puskesmas'));
         } else {
             $this->Mutu_Model->tambahData();
             $this->session->set_flashdata('flash', 'Ditambahkan');
-            redirect('Organisasi_Manajemen');
+            redirect('Mutu');
         }
     }
-    public function ubah()
+    public function ubah($kode)
     {
-        $data['data'] = $this->Mutu_Model->check();
-        $this->load->view('templates/puskesmas/head');
-        $this->load->view('puskesmas/organoisasi_manajemen', $data);
-        $this->load->view('templates/puskesmas/foot');
+        $data['judul'] = 'Edit Peningkatan Mutu Puskesmas';
+        $data['data'] = $this->Mutu_Model->check($kode);
+        $data['kode_puskesmas'] = $kode;
+
+        $this->form_validation->set_rules('kode_puskesmas', '', 'trim|required');
+        $this->form_validation->set_rules('peningkatan1', '', 'trim|in_list[1,2,3]');
+        $this->form_validation->set_rules('peningkatan2', '', 'trim|in_list[1,2,3]');
+        $this->form_validation->set_rules('peningkatan3', '', 'trim|in_list[1,2,3]');
+        $this->form_validation->set_rules('peningkatan4', '', 'trim|in_list[1,2,3]');
+        $this->form_validation->set_rules('peningkatan5', '', 'trim|in_list[1,2,3]');
+
+        if ($this->form_validation->run() == false) {
+            $this->load->view('templates/kab_kota/head', $data);
+            $this->load->view('kab_kota/mutu', $data);
+            $this->load->view('templates/kab_kota/foot');
+        } else {
+            $this->Mutu_Model->ubahData();
+            $this->session->set_flashdata('flash', 'Diubah');
+            redirect('Mutu');
+        }
     }
 }
--- application/models/Mutu_Model.php
+++ application/models/Mutu_Model.php
@@ -6,18 +6,39 @@ class Mutu_Model extends CI_Model
     public function tambahData()
     {
         $data = [
-            "kode" => $this->session->userdata('kode'),
+            "kode" => $this->input->post('kode_puskesmas'),
             "indikator_mutu" => $this->input->post('peningkatan1', true),
             "audit_internal" => $this->input->post('peningkatan2', true),
             "rapat_tinjauan" => $this->input->post('peningkatan3', true),
             "melaksanakan_pps" => $this->input->post('peningkatan4', true),
-            "pelaporan_insiden" => $this->input->post('peningkatan5', true)
+            "pelaporan_insiden" => $this->input->post('peningkatan5', true),
+            "input_by" => $this->session->userdata('kode')
 
         ];
-        $this->db->insert('lokasi_puskesmas', $data);
+        $this->db->insert('peningkatan_mutu', $data);
     }
-    public function check()
+    public function check($kode)
     {
-        return $this->db->get_where('lokasi_puskesmas', ['kode' => $this->session->userdata('kode')])->row_array();
+        return $this->db->get_where('peningkatan_mutu', ['kode' => $kode])->row_array();
+    }
+    public function check_puskesmas($kab_kota)
+    {
+        $this->db->where('kab_kota', $kab_kota);
+        $this->db->where('role_id', '3');
+        return $this->db->get('user')->result_array();
+    }
+
+    public function ubahData()
+    {
+        $data = [
+            "indikator_mutu" => $this->input->post('peningkatan1', true),
+            "audit_internal" => $this->input->post('peningkatan2', true),
+            "rapat_tinjauan" => $this->input->post('peningkatan3', true),
+            "melaksanakan_pps" => $this->input->post('peningkatan4', true),
+            "pelaporan_insiden" => $this->input->post('peningkatan5', true),
+            "input_by" => $this->session->userdata('kode')
+        ];
+        $this->db->where('kode', $this->input->post('kode_puskesmas'));
+        $this->db->update('peningkatan_mutu', $data);
     }
 }
