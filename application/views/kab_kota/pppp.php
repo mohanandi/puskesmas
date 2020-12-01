087363ee811dee412d931eac75f7a7081a114581
iqbaladhis <iqbaladhis@gmail.com>
2020-12-01 10:40:07 +0700

    .

--- application/views/kab_kota/pppp.php
+++ application/views/kab_kota/pppp.php
@@ -41,16 +41,16 @@
         <br>
         <span><strong> Penilaian</strong> </span>
         <p style="font-size:12px;">
-          1. Bila aspek pelayanan kesehatan ≥ 91% dan cakupan hasil manajemen ≥ 8,5 , nilai 10 <br>
-          2. Bila aspek pelayanan kesehatan < 91%, hasil manajemen ≥ 8,5 atau aspek pelayanan kesehatan ≥ 91% dan hasil manajemen < 8,5 , nilai 5 <br>
-            3. Bila aspek pelayanan kesehatan < 91%, hasil manajemen < 8,5 , nilai 0 <p id="foo1" style="display:none; font-size:12px; margin-left:20px;">1. Dinas kesehatan kabupaten/kota melakukan verifikasi dan memberikan umpan balik terhadap penilaian kinerja Puskesmas dalam bentuk tertulis setiap akhir tahun berjalan atau pada awal tahun berikutnya</p>
-        <select class="form-control" name="pengawasan2" id="pengawasan2">
-          <?php is_terisi($data['feedback'], set_value('pengawasan2')); ?>
-          <option value="1">1</option>
-          <option value="2">2</option>
-          <option value="3">3</option>
-        </select>
-        <?= form_error('pengawasan2', '<small class="text-danger pl-3">', '</small>'); ?>
+          1. Bila terdapat umpan balik secara tertulis dan tepat waktu, nilai 10 <br>
+          2. Bila terdapat umpan balik secara tertulis tetapi tidak tepat waktu, nilai 5 <br>
+          3. Bila tidak terdapat umpan balik secara tertulis, nilai 0 <p id="foo1" style="display:none; font-size:12px; margin-left:20px;">1. Dinas kesehatan kabupaten/kota melakukan verifikasi dan memberikan umpan balik terhadap penilaian kinerja Puskesmas dalam bentuk tertulis setiap akhir tahun berjalan atau pada awal tahun berikutnya</p>
+          <select class="form-control" name="pengawasan2" id="pengawasan2">
+            <?php is_terisi($data['feedback'], set_value('pengawasan2')); ?>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+          </select>
+          <?= form_error('pengawasan2', '<small class="text-danger pl-3">', '</small>'); ?>
       </div>
 
       <button type="submit" class="btn float-right btn-success" href="table.html" style="color: white; margin-left:10px; margin-top:5px; border-radius:5px;"><?= $button; ?></button>
--- application/views/kab_kota/pppp_hasil.php
+++ application/views/kab_kota/pppp_hasil.php
@@ -24,10 +24,10 @@
         <br>
         <span><strong> Penilaian</strong> </span>
         <p style="font-size:12px;">
-          1. Bila aspek pelayanan kesehatan ≥ 91% dan cakupan hasil manajemen ≥ 8,5 , nilai 10 <br>
-          2. Bila aspek pelayanan kesehatan < 91%, hasil manajemen ≥ 8,5 atau aspek pelayanan kesehatan ≥ 91% dan hasil manajemen < 8,5 , nilai 5 <br>
-            3. Bila aspek pelayanan kesehatan < 91%, hasil manajemen < 8,5 , nilai 0 <p id="foo1" style="display:none; font-size:12px; margin-left:20px;">1. Dinas kesehatan kabupaten/kota melakukan verifikasi dan memberikan umpan balik terhadap penilaian kinerja Puskesmas dalam bentuk tertulis setiap akhir tahun berjalan atau pada awal tahun berikutnya</p>
-        <input type="text" class="form-control" value="<?= $data['feedback']; ?>" placeholder="" name="pengawasan2" id="pengawasan2" readonly>
+          1. Bila terdapat umpan balik secara tertulis dan tepat waktu, nilai 10 <br>
+          2. Bila terdapat umpan balik secara tertulis tetapi tidak tepat waktu, nilai 5 <br>
+          3. Bila tidak terdapat umpan balik secara tertulis, nilai 0 <p id="foo1" style="display:none; font-size:12px; margin-left:20px;">1. Dinas kesehatan kabupaten/kota melakukan verifikasi dan memberikan umpan balik terhadap penilaian kinerja Puskesmas dalam bentuk tertulis setiap akhir tahun berjalan atau pada awal tahun berikutnya</p>
+          <input type="text" class="form-control" value="<?= $data['feedback']; ?>" placeholder="" name="pengawasan2" id="pengawasan2" readonly>
 
 
       </div>
